3d15b938d564151179c6b3ad338fdb3c63ca8922
RiccardoCacio <riccardocacio@users.noreply.github.com>
2024-08-27 11:53:25 +0200

    .

--- portfolio/src/Components/Project/Main/MainProject.jsx
+++ portfolio/src/Components/Project/Main/MainProject.jsx
@@ -8,6 +8,8 @@ import Yourworld from '../../../Img/copertineProgetti/yourworldimg.png'
 import CityStats from '../../../Img/copertineProgetti/cityS.png'
 import ShoppingList from '../../../Img/copertineProgetti/shoppingList.png'
 import Pokedex from '../../../Img/copertineProgetti/Pokedex.png'
+import LaCasaDiEva from '../../../Img/copertineProgetti/LaCasaDiEva.png'
+
 
 import ProjectBox from './ProjectBox'
 
@@ -54,10 +56,10 @@ const MainProject = () => {
     const randompizzaP = 'This is my first project! This WebApp consinst in random consists of a random pizza generator to help the user in his choice. '
     const randomcolorP = "In this project I created a random color generator in HEX scale. I promise it's the last 'random' project!"
     const gameboyP = "I developed this counter for the Start2Impact Javascript test, inspired by my passion for video games."
-    const citystatsP = "I developed CityStats for the Start2Impact advanced Javascript test. This WebApp allows you to collect data on the quality of life in the most famous cities via API calls."
     const shoppinglistP = "This to-do-list is a personal project created to help users with their shopping list. To ensure that users do not lose their shopping list, it is saved in the device's local storage!"
     const yourworldP = "YourWorld is the latest project of the Start2Impact FrontEnd Developer Master. This 'SPA' allows the user to have all the latest data regarding Climate Change! For me this wasn't a simple project but a real campaign to raise awareness of the topics covered."
     const pokedexP = "Welcome to my Pokédex project made with React and Tailwind! In this project, I created a web application that acts as a digital Pokédex, allowing users to search for Pokémon and view details such as the Pokémon's picture and some key statistics."
+    const lacasadievaP = "I developed a modern, user-friendly website for La Casa di Eva B&B using WordPress and Elementor. The design highlights the B&B's charm, featuring easy booking options and a gallery that showcases the beautiful location. My focus was on creating a seamless online experience that appeals to potential guests."
     return (
         <main>
             <div className='flex flex-wrap justify-around gap-4 mt-[150px] ml-[10%] mr-[10%] max-md:flex-col items-center max-md:mt-[100px] max-md:mr-[2%] max-md:ml-[2%]'>
@@ -97,18 +99,7 @@ const MainProject = () => {
 
                 <div data-aos="flip-down">
                     <ProjectBox
-                        ProjectTitle={'04 City Stats'}
-                        ProjectImg={CityStats}
-                        ProjectAlt={'City Stats Img'}
-                        ProjectParagraph={citystatsP}
-                        ProjectTech={'Html, Css, Javascript, Axios'}
-                        ProjectLink={'https://riccardocacio.github.io/citystats/'}
-                    />
-                </div>
-
-                <div data-aos="flip-down">
-                    <ProjectBox
-                        ProjectTitle={'05 Shopping List'}
+                        ProjectTitle={'04 Shopping List'}
                         ProjectImg={ShoppingList}
                         ProjectAlt={'Shopping List Img'}
                         ProjectParagraph={shoppinglistP}
@@ -119,7 +110,7 @@ const MainProject = () => {
 
                 <div data-aos="flip-down">
                     <ProjectBox
-                        ProjectTitle={'06 Yourworld'}
+                        ProjectTitle={'05 Yourworld'}
                         ProjectImg={Yourworld}
                         ProjectAlt={'Yourworld Img'}
                         ProjectParagraph={yourworldP}
@@ -130,12 +121,23 @@ const MainProject = () => {
 
                 <div data-aos="flip-down">
                     <ProjectBox
-                        ProjectTitle={'07 Pokedex'}
+                        ProjectTitle={'06 Pokedex'}
                         ProjectImg={Pokedex}
                         ProjectAlt={'Pokedex Img'}
                         ProjectParagraph={pokedexP}
                         ProjectTech={'Html, React, JSX, Tailwind, Axios'}
                         ProjectLink={'https://pokedexriccardoc.netlify.app/'}
+                    />
+                </div>
+
+                <div data-aos="flip-down">
+                    <ProjectBox
+                        ProjectTitle={'07 La Casa Di Eva'}
+                        ProjectImg={LaCasaDiEva}
+                        ProjectAlt={'La Casa Di Eva Img'}
+                        ProjectParagraph={lacasadievaP}
+                        ProjectTech={'WordPress, Elementor'}
+                        ProjectLink={'https://lacasadievaumbria.it'}
                     />
                 </div>
             </div>
